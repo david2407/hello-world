392b8a8ede825ac3215dfab5be28b0a618e001a4
David Cortes <davidcortes2407@gmail.com>
2017-03-21 11:31:22 -0500

    Created file program.js

---
+++ program.js
@@ -0,0 +1,7 @@
+console.log(process.argv);
+var fs=require('fs');
+
+var buffer=fs.readFileSync('D:/David Cortes/Documentos/Capacitacion Angular/bower.json')
+var r=buffer.toString();
+r=r.split('\n');
+console.log(process.argv, r);
